6b9a1b5f067e7b1e7bf3249391815172be50ef11
Salman Ahmad <ahmadsalman9939@gmail.com>
2026-05-21 16:28:42 +0530

    updated website

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'inject-entrypoint',
-      transformIndexHtml(html, ctx) {
-        // For development server, inject entrypoint dynamically into HTML stream
-        if (ctx.server) {
-          return html.replace(
-            '</body>',
-            '<script type="module" src="/src/main.jsx"></script></body>'
-          );
-        }
-        return html;
-      }
-    }
-  ],
-  base: '/Tool-Trove/'
+  plugins: [react()],
+  base: '/Tool-Trove/',
 })
-
-
-
