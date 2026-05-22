92003d1797bc3e363b74ce9c25356317ce960006
Salman Ahmad <ahmadsalman9939@gmail.com>
2026-05-22 15:21:19 +0530

    Update workflow and configs

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Tool-Trove/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('scheduler') || id.includes('react-dom') || id.includes('react-router')) {
-              return 'vendor-react';
-            }
-            if (id.includes('framer-motion')) {
-              return 'vendor-framer';
-            }
-            if (id.includes('lucide-react')) {
-              return 'vendor-lucide';
-            }
-            return 'vendor-utils';
-          }
-        }
-      }
-    }
-  }
 })
